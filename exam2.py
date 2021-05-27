74673c1498850111d36f4533f34a98570495149b
onderyilmz <onder.yilmz@gmail.com>
2021-05-28 00:20:00 +0300

    Add files via upload

---
+++ exam2.py
@@ -0,0 +1,42 @@
+
+
+soz1 = {
+    'id': 1,
+    'name': 'Onder',
+    'job': 'Engineer',
+    }
+
+
+soz2 = {
+    'id': 2,
+    'name': 'Mustafa',
+    'job': 'Leader',
+    }
+
+
+soz3 = {
+    'id': 3,
+    'name': 'Kemal',
+    'job': 'Soldier',
+    }
+
+
+soz4 = {
+    'id': 4,
+    'name': 'ATATÜRK',
+    'job': 'HERŞEY',
+    }
+
+
+soz5 = {
+    'id': 5,
+    'name': 'Türkiye',
+    'job': 'BMM',
+    }
+
+
+print(soz1)
+print(soz2)
+print(soz3)
+print(soz4)
+print(soz5)
